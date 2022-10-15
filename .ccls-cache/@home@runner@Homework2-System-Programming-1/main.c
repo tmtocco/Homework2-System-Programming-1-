b2fc60fce4c8aaa4dcdac26740015a9ecf160a34
tmtocco <98406643+tmtocco@users.noreply.github.com>
2022-10-15 04:17:19 +0000

    Got to the point where I need to implement the Caesar method. Wrote everything from the txt file to a char array. Now I just have to encrypt everything and put it in another txt file

--- .ccls-cache/@home@runner@Homework2-System-Programming-1/main.c
+++ .ccls-cache/@home@runner@Homework2-System-Programming-1/main.c
@@ -7,33 +7,43 @@ int main(int argc, char* argv[]) {
   char* pathExisting = argv[1];
   char* pathCreate = argv[2];
   int offset = atoi(argv[3]);
+  char* dataToBeRead = malloc(sizeof(char[1000]));
 
   printf("Existing path: %s\n", pathExisting);
   printf("New path: %s\n", pathCreate);
-  printf("Offset: %d\n",offset);
+  printf("Offset: %d\n\n",offset);
   
-  filePointer = fopen(pathExisting, "w");
+  filePointer = fopen(pathExisting, "r");
   
   if(filePointer == NULL) {
     printf("Error found within existing path! Exiting program.\n");
     exit(0);
   } else {
+    printf("Contents within %s: \n", pathExisting);
     
-  }
-  /*
-  if() {
-    
-    printf("Error found within creating path! Exiting program.");
-  }
-  */
-  if(offset > 25 || offset < -25) {
-    printf("Error found within offset value! Exiting program.\n");
-    exit(0);
+    while (fgets(dataToBeRead, 1000, filePointer) != NULL) {
+      printf("%s\n\n", dataToBeRead);
+    }
   }
   
-  printf("pathname of existing file: ");
-  scanf("%s", pathExisting);
-
+  filePointer = fopen(pathCreate, "w");
+  
+  if(filePointer == NULL) {
+    printf("Error found within creating path! Exiting program.");
+    exit(1);
+    
+  } else if(offset > 25 || offset < -25) {
+    printf("Error found within offset value! Exiting program.\n");
+    exit(2);
+    
+  } else {
+    char* encryptedData = malloc(sizeof(dataToBeRead));
+    
+    for (int i=0; i < sizeof(dataToBeRead); i++) {
+      encryptedData[i] = caesar(dataToBeRead[i], offset);  
+    }
+  }
+  
   
   
   return 0;
